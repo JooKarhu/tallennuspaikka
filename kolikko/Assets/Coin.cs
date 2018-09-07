7fbdd363d4edf27a94f085d4e255602d5bb539d6
Joonas Henrik Karhu <s6kajo02@students.osao.fi>
2018-09-07 10:56:59 +0300

    kaka
    yritys

---
+++ kolikko/Assets/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    void Start()
+    {
+        Controller.maxCoins++;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right * 128 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        OnDestroy();
+        Controller.coins ++;
+    }
+
+    void OnDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ kolikko/Assets/WinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScript : MonoBehaviour 
+{
+    public Text winText;
+
+    void Awake()
+    {
+        winText.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Controller.coins <= 0)
+        {
+            winText.enabled = true;
+        }
+    }
+}
